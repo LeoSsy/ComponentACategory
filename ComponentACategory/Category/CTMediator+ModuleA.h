a55220f6720bc00643e3fafd7e207e3a74e40d9c
Jack <781739973@qq.com>
2018-11-09 14:14:37 +0800

    初始化工程

--- ComponentACategory/Category/CTMediator+ModuleA.h
+++ ComponentACategory/Category/CTMediator+ModuleA.h
@@ -1,15 +1,13 @@
 //
-//  CTMediator+ModuleB.h
-//  ModuleB-Category
+//  CTMediator+ModelA.h
+//  ComponentACategory
 //
-//  Created by 刘光强 on 2018/10/1.
-//  Copyright © 2018年 quangqiang. All rights reserved.
+//  Created by Jack on 2018/11/9.
+//  Copyright © 2018年 bianla. All rights reserved.
 //
 
 #import "CTMediator.h"
 
 @interface CTMediator (ModuleA)
-
 - (UIViewController *)ModuleB_viewControllerWithCallback:(void(^)(NSString *result))callback;
-
 @end
--- ComponentACategory/Category/CTMediator+ModuleA.m
+++ ComponentACategory/Category/CTMediator+ModuleA.m
@@ -1,19 +1,17 @@
 //
-//  CTMediator+ModuleB.m
-//  ModuleB-Category
+//  CTMediator+ModelA.m
+//  ComponentACategory
 //
-//  Created by 刘光强 on 2018/10/1.
-//  Copyright © 2018年 quangqiang. All rights reserved.
+//  Created by Jack on 2018/11/9.
+//  Copyright © 2018年 bianla. All rights reserved.
 //
 
-#import "CTMediator+ModuleB.h"
+#import "CTMediator+ModelA.h"
 
 @implementation CTMediator (ModuleA)
-
 - (UIViewController *)ModuleB_viewControllerWithCallback:(void(^)(NSString *result))callback {
     NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
     params[@"callback"] = callback;
     return [self performTarget:@"ModuleA" action:@"viewController" params:params shouldCacheTarget:NO];
 }
 @end
-    
